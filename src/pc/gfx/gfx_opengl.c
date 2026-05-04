44abfe1f10170d81d2842a188eee096430b6d61a
Murat Kaan Tekeli <230502007@kocaelisaglik.edu.tr>
2026-05-04 22:09:26 +0300

    add saturation uniform to OpenGL renderer

--- src/pc/gfx/gfx_opengl.c
+++ src/pc/gfx/gfx_opengl.c
@@ -604,6 +604,9 @@ static void gfx_opengl_set_use_alpha(bool use_alpha) {
 
 static void gfx_opengl_draw_triangles(float buf_vbo[], size_t buf_vbo_len, size_t buf_vbo_num_tris) {
     //printf("flushing %d tris\n", buf_vbo_num_tris);
+    if (opengl_prg != NULL) {
+        glUniform1f(opengl_prg->uniform_locations[5], gColorSaturation);
+    }
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * buf_vbo_len, buf_vbo, GL_STREAM_DRAW);
     glDrawArrays(GL_TRIANGLES, 0, 3 * buf_vbo_num_tris);
 }
